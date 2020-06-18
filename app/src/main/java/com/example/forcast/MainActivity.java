795b22fb18da0380973a306ebf1f39d37930f364
Serg <sergivnik@mail.ru>
2020-06-18 18:14:15 +0300

    project Forcast

--- app/src/main/java/com/example/forcast/MainActivity.java
+++ app/src/main/java/com/example/forcast/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.forcast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void sendMessage(View view) {
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.textView3);
+        textView.setText(message);
+    }
+    public void changeCity(View view) {
+        setContentView(R.layout.choise_city);
     }
 }
