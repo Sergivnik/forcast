8a359fe7a9dbb5627be0bbf4f60ee9c604ae5a01
Serg <sergivnik@mail.ru>
2020-06-20 14:22:18 +0300

    project Forcast

--- app/src/main/java/com/example/forcast/MainActivity.java
+++ app/src/main/java/com/example/forcast/MainActivity.java
@@ -6,20 +6,88 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private String message;
+    private String temperature;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String instanceState;
+        if (savedInstanceState == null){
+            instanceState = "Первый запуск!";
+        }
+        else{
+            instanceState = "Повторный запуск!";
+        }
+        Toast.makeText(getApplicationContext(), instanceState + " - onCreate()", Toast.LENGTH_SHORT).show();
     }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle saveInstanceState){
+        super.onRestoreInstanceState(saveInstanceState);
+        Toast.makeText(getApplicationContext(), "Повторный запуск!! - onRestoreInstanceState()", Toast.LENGTH_SHORT).show();
+        message = saveInstanceState.getString("City");
+        TextView textView = (TextView) findViewById(R.id.textView3);
+        textView.setText(((String)message).toString());
+        temperature = saveInstanceState.getString("Temperature");
+        TextView textView1 = (TextView) findViewById(R.id.textView);
+        textView1.setText(((String)temperature).toString());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(getApplicationContext(), "onResume()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(getApplicationContext(), "onPause()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle saveInstanceState){
+        super.onSaveInstanceState(saveInstanceState);
+        Toast.makeText(getApplicationContext(), "onSaveInstanceState()", Toast.LENGTH_SHORT).show();
+        saveInstanceState.putString("City",message);
+        saveInstanceState.putString("Temperature",temperature);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(getApplicationContext(), "onStop()", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(getApplicationContext(), "onRestart()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(getApplicationContext(), "onDestroy()", Toast.LENGTH_SHORT).show();
+    }
+
     public void sendMessage(View view) {
         EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
+        message = editText.getText().toString();
+        EditText editText1 = (EditText) findViewById(R.id.edit_message2);
+        temperature=editText1.getText().toString();
+        temperature=temperature+"° C";
         setContentView(R.layout.activity_main);
-        TextView textView = (TextView) findViewById(R.id.textView3);
-        textView.setText(message);
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(temperature);
+        TextView textView1 = (TextView) findViewById(R.id.textView3);
+        textView1.setText(message);
     }
     public void changeCity(View view) {
         setContentView(R.layout.choise_city);
